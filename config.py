0186d72da9e902c30252516ec5c9c4ab0f315bca
mohammedfaddel <ic20b009@technikum-wien.at>
2024-01-14 19:10:42 +0100

    Kafka and DB part are done

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+    'bootstrap.servers': 'pkc-w7d6j.germanywestcentral.azure.confluent.cloud:9092',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanisms': 'PLAIN',
+    'sasl.username': 'JIWMTU2LEVSGB7J5',
+    'sasl.password': 'iyLG8FxzawLEVebXC2hnIAJh88mE6nWqvc4mLGR80j+aU6OIEEgx2mX9UHcMkEvK'}
+
+# Config for Schema Registry
+sr_config = {
+    'url': 'https://psrc-lg26v.westeurope.azure.confluent.cloud',
+    'basic.auth.user.info':'WLERFXG7K6QDXSGH:WxaSe8hjmpZ/8ichPrCC6A0HIgnst0zWB3dKQ1rcsuyWZVivdyyCa0j+XwnFGkGU'
+}

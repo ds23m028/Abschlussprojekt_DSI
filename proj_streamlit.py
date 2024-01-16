66c77a4374e966066c65cb2ffd3b61ee348fd1f1
mohammedfaddel <ic20b009@technikum-wien.at>
2024-01-16 19:17:36 +0100

    Streamlit updated

--- proj_streamlit.py
+++ proj_streamlit.py
@@ -2,13 +2,23 @@ import streamlit as st
 import pandas as pd
 import json
 import plotly.express as px
+from pymongo import MongoClient
+
 
 @st.cache_data
 def load_data():
-    data = pd.read_csv('clean_vie_data.csv').set_index('Unnamed: 0')
-    data['DISTRICT_CODE'] = data['DISTRICT_CODE'].astype(str).str[:3]
+    try:
+        client = MongoClient("mongodb://localhost:27017/")
+        print("connected successfully")
+    except:
+        print ("could not connect to mongoDB")
+    db_Mongo = client.get_database('DSI_Abschlussprojekt')
+    collection= db_Mongo.Geburten_nach_Einkommen_Wien
+    df = pd.DataFrame(list(collection.find()))
+    #data = pd.read_csv('clean_vie_data.csv').set_index('Unnamed: 0')
+    df['District_code'] = df['District_code'].astype(str).str[:3]
     #print(data)
-    return data
+    return df
 
 
 def render_info():
@@ -18,8 +28,8 @@ def render_info():
 
 def filter_by_year(data):
     # adding a range slider, allowing to choose values in range
-    range = st.slider('Year', min(data['REF_YEAR']), max(data['REF_YEAR']), (2002,2021))
-    return data[(data['REF_YEAR'].between(*range))]
+    range = st.slider('Year', min(data['Year']), max(data['Year']), (2002,2021))
+    return data[(data['Year'].between(*range))]
 
 def render_map(data):
     json_data = open('BEZIRKSGRENZEOGD (1).json', 'r')
@@ -29,52 +39,52 @@ def render_map(data):
 
     tab1, tab2 = st.tabs(["Total", "Male vs. Female"])
     with tab1:
-        fig_t = px.choropleth_mapbox(data, geojson=borders, color='INC_TOT_VALUE',
-                                   locations="DISTRICT_CODE", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
+        fig_t = px.choropleth_mapbox(data, geojson=borders, color='Total_Salary',
+                                   locations="District_code", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
                                    center={"lat": 48.210033, "lon": 16.363449},
                                    mapbox_style="carto-positron", zoom=9.8,
                                    opacity=0.5,
                                    color_continuous_scale = 'rdbu',
-                                   labels={"INC_TOT_VALUE": "Netto income"})
+                                   labels={"Total_Salary": "Netto income"})
         fig_t.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
         st.plotly_chart(fig_t, theme="streamlit", use_container_width=False)
     with tab2:
         col1, col2 = st.columns(2)
         with col1:
             st.write("Male")
-            fig_m = px.choropleth_mapbox(data, geojson=borders, color='INC_MAL_VALUE',
-                                        locations="DISTRICT_CODE", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
+            fig_m = px.choropleth_mapbox(data, geojson=borders, color='Male_Salary',
+                                        locations="District_code", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
                                         center={"lat": 48.210033, "lon": 16.363449},
                                         mapbox_style="carto-positron", zoom=9,
                                         opacity=0.5,
                                         color_continuous_scale = 'rdbu',
-                                        labels={"INC_MAL_VALUE": "Netto income"},
+                                        labels={"Male_Salary": "Netto income"},
                                         title='Male',
-                                        range_color =[min(data['INC_FEM_VALUE']), max(data['INC_MAL_VALUE'])])
+                                        range_color =[min(data['Female_Salary']), max(data['Male_Salary'])])
             fig_m.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
             st.plotly_chart(fig_m, theme="streamlit", use_container_width=True)
         with col2:
             st.write("Female")
-            fig_f = px.choropleth_mapbox(data, geojson=borders, color='INC_FEM_VALUE',
-                                         locations="DISTRICT_CODE", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
+            fig_f = px.choropleth_mapbox(data, geojson=borders, color='Female_Salary',
+                                         locations="District_code", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
                                          center={"lat": 48.210033, "lon": 16.363449},
                                          mapbox_style="carto-positron", zoom=9,
                                          opacity=0.5,
                                          color_continuous_scale = 'rdbu',
-                                         labels={"INC_FEM_VALUE": "Netto income"},
+                                         labels={"Female_Salary": "Netto income"},
                                          title='Female',
-                                         range_color =[min(data['INC_FEM_VALUE']), max(data['INC_MAL_VALUE'])])
+                                         range_color =[min(data['Female_Salary']), max(data['Male_Salary'])])
             fig_f.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
             st.plotly_chart(fig_f, theme="streamlit", use_container_width=True)
     st.subheader('Births by district of Vienna')
 
-    fig_b = px.choropleth_mapbox(data, geojson=borders, color='BIR',
-                                 locations="DISTRICT_CODE", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
+    fig_b = px.choropleth_mapbox(data, geojson=borders, color='Births',
+                                 locations="District_code", featureidkey="properties.STATAUSTRIA_BEZ_CODE",
                                  center={"lat": 48.210033, "lon": 16.363449},
                                  mapbox_style="carto-positron", zoom=9.8,
                                  opacity=0.5,
                                  color_continuous_scale = 'rdbu',
-                                 labels={"BIR": "Birth rate"})
+                                 labels={"Births": "Birth rate"})
     fig_b.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     st.plotly_chart(fig_b, theme="streamlit", use_container_width=True)
 
